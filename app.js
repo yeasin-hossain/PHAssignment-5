853c14dc2ec65d9594cfad65545e768966c7cf1e
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-02-07 05:08:55 +0600

    now i can sleep

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const singleItem = (e) => {
 
 //All recipe render function
 const recipeRender = (recipes) => {
-	// map the array and render to dom and singleItem function call for render single item
+	// map the array and render to dom and "singleItem" function call for render single item
 	const recipeItem = recipes
 		.map((recipe) => {
 			return `
@@ -52,11 +52,26 @@ const recipeRender = (recipes) => {
 
 // single recipe render function
 const singleRecipeRender = (recipes) => {
+	// in strMeasures has null and empty value that's why i filter it using for loop.
+	const strMeasures = recipes[0];
+	const strMeasure = [];
+	for (let i = 1; i <= 20; i++) {
+		if (strMeasures[`strIngredient${i}`]) {
+			strMeasure.push(
+				`<li class="list-group-item">✔ ${strMeasures[`strMeasure${i}`]} ${
+					strMeasures[`strIngredient${i}`]
+				}</li>`
+			);
+		}
+	}
+
 	const recipeItem = recipes
 		.map((recipe) => {
 			return `
             <div class="card border-0 p-3" style="width: 80%;">
-                <img src="${recipe.strMealThumb}" class="card-img-top d-block m-auto"style="height:250px; width:250px" alt="...">
+                <img src="${
+									recipe.strMealThumb
+								}" class="card-img-top d-block m-auto"style="height:250px; width:250px" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${recipe.strMeal}</h5>
                     <p class="card-text">${recipe.strCategory}</p>
@@ -64,26 +79,7 @@ const singleRecipeRender = (recipes) => {
                 </div>
                 <ul class="list-group list-group-flush">
                 <h6 class="card-text">Ingredients</h6>
-                    <li class="list-group-item">✔ ${recipe.strMeasure1} ${recipe.strIngredient1}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure2} ${recipe.strIngredient2}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure3} ${recipe.strIngredient3}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure4} ${recipe.strIngredient4}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure5} ${recipe.strIngredient5}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure6} ${recipe.strIngredient6}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure7} ${recipe.strIngredient7}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure8} ${recipe.strIngredient8}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure9} ${recipe.strIngredient9}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure10} ${recipe.strIngredient10}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure11} ${recipe.strIngredient11}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure12} ${recipe.strIngredient12}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure13} ${recipe.strIngredient13}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure14} ${recipe.strIngredient14}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure15} ${recipe.strIngredient15}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure16} ${recipe.strIngredient16}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure17} ${recipe.strIngredient17}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure18} ${recipe.strIngredient18}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure19} ${recipe.strIngredient19}</li>
-                    <li class="list-group-item">✔ ${recipe.strMeasure20} ${recipe.strIngredient20}</li>
+                    ${strMeasure.join('')}
                 </ul>
                 <button class="btn btn-info" onClick="modalClose()">Close</button>
             </div>
