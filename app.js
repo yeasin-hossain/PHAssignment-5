c739f76c7a92f48106f89b6061cbf68eb114cc21
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-02-07 04:18:37 +0600

    done initial

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ const searchButton = document.querySelector('#search__button');
 
 // fetch item
 const fetchRecipe = (recipeName) => {
-	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`)
-		.then((res) => res.json())
-		.then((data) => {
-			if (data.meals != null) {
-				recipeRender(data.meals);
-			} else {
-				message(
-					`<h4>😢 😢 Sorry <span class="text-danger"> ${recipeName} </span> is not Available! please Try Something else!</h4>`
-				);
-			}
-		});
+	// filter input empty or not
+	if (recipeName.length >= 1) {
+		fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`)
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.meals != null) {
+					recipeRender(data.meals);
+				} else {
+					message(
+						`<h4>😢 😢 Sorry <span class="text-danger"> ${recipeName} </span> is not Available! please Try Something else!</h4>`
+					);
+				}
+			});
+	} else {
+		message(`<h4>😢 😢 Your recipe name Please!!!</h4>`);
+	}
 };
+
 // fetch single item
 const singleItem = (e) => {
+	// call single item by id
 	fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e}`)
 		.then((res) => res.json())
 		.then((data) => {
@@ -26,6 +33,7 @@ const singleItem = (e) => {
 
 //All recipe render function
 const recipeRender = (recipes) => {
+	// map the array and render to dom and singleItem function call for render single item
 	const recipeItem = recipes
 		.map((recipe) => {
 			return `
@@ -39,7 +47,7 @@ const recipeRender = (recipes) => {
 		})
 		.join('');
 	document.querySelector('.content__section').innerHTML = recipeItem;
-	console.log(recipes);
+	spinner('off');
 };
 
 // single recipe render function
@@ -47,8 +55,8 @@ const singleRecipeRender = (recipes) => {
 	const recipeItem = recipes
 		.map((recipe) => {
 			return `
-            <div class="card border-0" style="width: 80%;">
-                <img src="${recipe.strMealThumb}" class="card-img-top w-50 h-25 d-block m-auto" alt="...">
+            <div class="card border-0 p-3" style="width: 80%;">
+                <img src="${recipe.strMealThumb}" class="card-img-top d-block m-auto"style="height:250px; width:250px" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${recipe.strMeal}</h5>
                     <p class="card-text">${recipe.strCategory}</p>
@@ -76,20 +84,20 @@ const singleRecipeRender = (recipes) => {
                     <li class="list-group-item">✔ ${recipe.strMeasure18} ${recipe.strIngredient18}</li>
                     <li class="list-group-item">✔ ${recipe.strMeasure19} ${recipe.strIngredient19}</li>
                     <li class="list-group-item">✔ ${recipe.strMeasure20} ${recipe.strIngredient20}</li>
-
-                   
                 </ul>
-
+                <button class="btn btn-info" onClick="modalClose()">Close</button>
             </div>
         `;
 		})
 		.join('');
-	document.querySelector('.content__section').innerHTML = recipeItem;
+	// there is 2 option render single item i'm choice modal option, you can try 2nd option by uncomment "content__section"
+	messageModal(recipeItem);
+	// document.querySelector('.content__section').innerHTML = recipeItem;
 };
 
 // Enter key event
 item__name.addEventListener('keyup', (e) => {
-	e.keyCode === 13 ? fetchRecipe(e.target.value) : 'sorry';
+	e.keyCode === 13 ? (fetchRecipe(e.target.value), spinner('open')) : 'sorry';
 });
 // Button event
 searchButton.addEventListener('click', () => {
@@ -100,10 +108,49 @@ searchButton.addEventListener('click', () => {
 const message = (msg) => {
 	const msgDom = document.querySelector('.error__message');
 	msgDom.innerHTML = msg;
-	msgDom.classList.add('d-block');
-	msgDom.classList.remove('d-none');
+
+	displayToggle('.error__message', 'block');
 	setTimeout(() => {
-		msgDom.classList.add('d-none');
-		msgDom.classList.remove('d-block');
+		displayToggle('.error__message', 'none');
 	}, 3000);
+};
+
+// single item  Modal
+const messageModal = (e) => {
+	const modal = document.querySelector('.message__modal');
+	modal.innerHTML = e;
+	displayToggle('.message__modal', 'flex');
+	displayToggle('.content__section', 'none');
+};
+
+// Modal Close
+const modalClose = () => {
+	displayToggle('.message__modal', 'none');
+	displayToggle('.content__section', 'block');
+	document.querySelector('.content__section').classList.add('d-flex');
+};
+
+// Display Toggle
+const displayToggle = (content, type) => {
+	const contentName = document.querySelector(content);
+	if (type == 'none') {
+		contentName.classList.add('d-none');
+		contentName.classList.remove('d-block');
+		contentName.classList.remove('d-flex');
+	} else if (type == 'flex') {
+		contentName.classList.add('d-flex');
+		contentName.classList.remove('d-none');
+	} else {
+		contentName.classList.remove('d-none');
+		contentName.classList.add('d-block');
+	}
+};
+
+// spinner
+const spinner = (spinType) => {
+	if (spinType == 'open') {
+		displayToggle('#spinner', 'flex');
+	} else {
+		displayToggle('#spinner', 'none');
+	}
 };
